80728bc50d0fa8e65cabbaaa5b848b2b06ce59e6
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-08-03 14:52:03 +0100

    basic runApp and display text

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp()); // alternative, one line code.
 
 class MyApp extends StatelessWidget {
+  @override // override build method (Stylistic things)
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Text(
-      'Hello!',
+      'Hello!!!!',
     ));
   }
 }
